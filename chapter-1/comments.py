41e27e54ff1d8f52124c92823be00c099ef70ff1
SHERI SHIVAJI <shivajisheri@gmail.com>
2025-04-13 15:33:41 +0530

    Single-line comment amd multi-line comment in python

---
+++ chapter-1/comments.py
@@ -0,0 +1,9 @@
+#This is single line comment
+print("This is single line comment")
+
+"""
+This is an amazing
+example of multi-line 
+comments!
+"""
+print("This is an amazing example of multi-line comments!")
